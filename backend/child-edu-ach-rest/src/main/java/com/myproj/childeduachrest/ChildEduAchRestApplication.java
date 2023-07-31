cc081e81786783b33f8af6cbfb0917761acec87f
Deyan Tsonev <dio.tsonev@gmail.com>
2023-07-31 19:54:04 +0300

    added initial spring boot

---
+++ backend/child-edu-ach-rest/src/main/java/com/myproj/childeduachrest/ChildEduAchRestApplication.java
@@ -0,0 +1,13 @@
+package com.myproj.childeduachrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildEduAchRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildEduAchRestApplication.class, args);
+	}
+
+}
---
+++ backend/child-edu-ach-rest/src/test/java/com/myproj/childeduachrest/ChildEduAchRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproj.childeduachrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildEduAchRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
